1c2174f451f37630efdca74f4d809127db597408
Çağrı Topçu <57417972+cgrtpc@users.noreply.github.com>
2021-05-01 23:20:27 +0300

    first commit

---
+++ interfaces/CustomerManager.java
@@ -0,0 +1,23 @@
+public class CustomerManager {
+
+    Logger[] loggers;
+    public CustomerManager(Logger[] logger){
+        this.loggers = logger;
+    }
+
+    public void add(Customer customer){
+        System.out.println("Müşteri eklendi " + customer.getFirstName());
+
+        for(Logger logger : loggers){
+            logger.log(customer.getFirstName());
+        }
+    }
+
+    public void delete(Customer customer){
+        System.out.println("Müşteri silindi " + customer.getFirstName());
+
+        for(Logger logger : loggers){
+            logger.log(customer.getFirstName());
+        }
+    }
+}
